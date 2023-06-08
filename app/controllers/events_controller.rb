eb06b3c7030b57412b2ea84fc78f2c0173f81520
Mark Muller <mark.muller.w@gmail.com>
2023-06-08 17:00:57 +0200

    create recurring events logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,30 +1,4 @@
 class EventsController < ApplicationController
 
-  def index
-    # Logic to fetch all events and render them
-  end
-
-  def show
-    # Logic to fetch a specific event and render it
-  end
-
-  def new
-    # Logic to create a new event and render the form
-  end
-
-  def create
-    # Logic to save a new event based on the submitted form data
-  end
-
-  def edit
-    # Logic to retrieve an existing event and render the edit form
-  end
-
-  def update
-    # Logic to update an existing event based on the submitted form data
-  end
-
-  def destroy
-    # Logic to delete an event
-  end
+   # update the status of the event (upcoming, overdue)
 end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,6 +1,6 @@
 class Habit < ApplicationRecord
   belongs_to :challenge
   belongs_to :user
-  has_many :events
+  has_many :events, dependent: :destroy
   validates :implementation_cycle, presence: true
 end
