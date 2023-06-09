018147c20e16f144bdf29ae599109388ddb496b7
Theresa Eberle <theresa.eberle@posteo.de>
2023-06-09 15:59:27 +0200

    first dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-  # For additional fields in app/views/devise/registrations/new.html.erb
+    # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name city age_range profile_description profile_picture])
 
-  # For additional in app/views/devise/registrations/edit.html.erb
+    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name city age_range profile_description profile_picture])
   end
 end
