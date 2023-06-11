610d6beb1a6c08839bf96f1a7cf42a493725966b
barbarapaszkiewicz <bar.paszkiewicz@gmail.com>
2023-06-11 08:59:24 +0200

    carousel extra changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'search_results', to: "search#index"
 
   devise_for :users
-  root to: "challenges#index"
+ # root to: "challenges#index"
+ 
+ # added landing page for the web app
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
