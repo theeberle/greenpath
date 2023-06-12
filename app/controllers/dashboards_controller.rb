c3c911d948a1be804af67d91d422581d7ae4fbd8
barbarapaszkiewicz <bar.paszkiewicz@gmail.com>
2023-06-12 11:57:35 +0200

    changing challenge to habit

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   def index
     @user = current_user
     @habits = current_user.habits
-    @user_challenges = @user.challenges
+    # change this to current_user.habits
+    @user_challenges = @user.habits
   end
 end
