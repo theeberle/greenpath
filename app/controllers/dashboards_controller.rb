0b88cd92cb3abc8f6f25e07bc0d8ee5598f39e3b
Theresa Eberle <theresa.eberle@posteo.de>
2023-06-12 13:04:54 +0200

    statistics saved

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,27 @@ class DashboardsController < ApplicationController
     @user = current_user
     @habits = current_user.habits
     @user_challenges = @user.challenges
+    # @user_challenges_habits = @habits.challenges
+    set_pie_chart_data
   end
+
+  private
+
+  # def set_pie_chart_data
+  #   @grouped_data = @habits.group(:category_id).count
+  #   @pie_data = []
+
+  #   @grouped_data.map do |category_id, count|
+  #     @pie_data << [Category.find(category_id).name, count]
+  #   end
+  # end
+
+  # def set_carbon_chart_data
+  #   @carbon_data = []
+  # end
+
+  # def set_event_chart_data
+  #   @event_data = []
+  # end
+
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,7 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.1/dist/sweetalert2.all.js"
+
+# For statistics (Chartkick)
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
