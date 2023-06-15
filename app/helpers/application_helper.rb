eb96cacb9844b6bfb00f3a2707a3890ab6156e27
barbarapaszkiewicz <bar.paszkiewicz@gmail.com>
2023-06-15 12:07:16 +0200

    backend & tree css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,19 +4,19 @@ module ApplicationHelper
     if carbon_savings == 0
       { icon: 'seedling',
         tree: "https://assets4.lottiefiles.com/packages/lf20_o32VvNhBlv.json",
-        level: "Blue Badge Owner, start your journey with a single step and unclock the Bronze Badge!'" }
+        level: "Blue Badge Owner, start your journey with a single step and unclock the Bronze Badge!" }
     elsif carbon_savings < 100
       { icon: 'award',
         tree: "https://assets4.lottiefiles.com/packages/lf20_e3ux72wx.json",
-        level: "You are on the Level 2. Reach over 100 points and unlock the Silver Badge!'"}
+        level: "You are on the Level 2. Reach over 100 points and unlock the Silver Badge!"}
     elsif carbon_savings > 100 && carbon_savings < 500
       { icon: 'medal',
-        tree: "https://assets4.lottiefiles.com/private_files/lf30_I6qCjk.json",
-        level: "You are on the Level 3. Reach over 500 points and unlock the Golden Badge.'"}
+        tree: "https://assets4.lottiefiles.com/private_files/lf30_vfgwrgqf.json",
+        level: "You are on the Level 3. Reach over 500 points and unlock the Golden Badge!"}
     else
       { icon: 'trophy',
-        tree: "https://assets8.lottiefiles.com/private_files/lf30_jdygihq2.json",
-        level: "You are on the Level 4! Golden Badge unlocked!.'" }
+        tree: "https://assets3.lottiefiles.com/private_files/lf30_jdygihq2.json",
+        level: "You are on the Level 4! Golden Badge unlocked!" }
     end
   end
 end
