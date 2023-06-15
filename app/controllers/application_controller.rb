53755615a927fb65af3a8a6eabebb372bd209260
barbarapaszkiewicz <bar.paszkiewicz@gmail.com>
2023-06-15 15:01:18 +0200

    metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name city age_range profile_description profile_picture])
   end
+  
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
